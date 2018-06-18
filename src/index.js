41db778a544768e8972512908e92bd3c129ee020
Adam Dickinson <adam@renegadedigital.com.au>
2018-06-19 01:23:12 +1000

    Define Schema
    - Add comprehensive schema

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import cors from 'cors'
+import { buildSchema } from "graphql"
+import cors from "cors"
 import express from "express"
 import graphqlHTTP from "express-graphql"
 
 import rootValue from "./resolvers"
-import schema from "./schema"
+import types from "./types"
 
+
+const schema = buildSchema(types)
 
 
 const app = express()
